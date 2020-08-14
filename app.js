5585dc2c9a0b75a537c59088ac8f86532f5385fa
Enzo <enzod98@outlook.com>
2020-08-14 14:54:03 -0400

    Preparada la sincronización galería con Instagram para producción. Se llevará a cabo cada 6 horas. TAmbién se reemplazó el archivo de config por un .env el cual se incluye en el gitignore para mayor seguridad

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-require('./functions/sincronizarGaleria');
+const sincronizarGaleria = require('./functions/sincronizarGaleria');
 
 //Inicializamos el servidor express
 app.use(require('./server/server'));
 
 //servicio de sincronización de galería
-//sincronizarGaleria();
+setInterval(sincronizarGaleria, 21600000);
+
--- functions/sincronizarGaleria.js
+++ functions/sincronizarGaleria.js
@@ -5,12 +5,21 @@ guardarFotos(posts);
 /* const axios = require("axios");
 
 axios({
-    aqui va el aux
+    "method":"GET",
+    "url":"https://easy-instagram-service.p.rapidapi.com/username",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"easy-instagram-service.p.rapidapi.com",
+    "x-rapidapi-key": process.env.RAPID_API_IG_KEY,
+    "useQueryString":true
+    },"params":{
+    "random":"x8n3nsj2",
+    "username":"esno23"
     }
     })
     .then((response)=>{
-        console.log(response);
-        let posts = response.last_post;
+       // console.log(response);
+        let posts = response.data.last_post;
         console.log(posts);
       guardarFotos(posts);
     })
@@ -20,8 +29,8 @@ axios({
 
     module.exports = axios;
     
-    
      */
+    
 //console.log(respuesta.last_post);
 
 
